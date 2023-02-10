629b07102674142557cc50de3e8724c5c79e2cf7
HyperGuru21 <94612217+HyperGuru21@users.noreply.github.com>
2023-02-10 16:47:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preLoad(){
+
+
+}
+
+function setup(){
+
+    canvas = createCanvas(300 , 300);
+    canvas.center();
+
+}
+
+function draw(){
+
+
+}
+
+function take_snapshot(){
+
+    save('mymustachefilter.png');
+
+}
